fb3e59a3469cd88cc8996fd5d4229418088f822b
Cesar Sanchez <csarsnchz@gmail.com>
2024-01-29 09:26:11 -0600

    Adding PORT on env configuration

--- cerp-api/src/main.ts
+++ cerp-api/src/main.ts
@@ -4,6 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api/v0.0.1');
-  await app.listen(5000);
+  await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
